2a4595d51762936dd8d99af7804c8181ce03506a
Rui Lopes <rgl@ruilopes.com>
2022-03-08 19:50:44 +0000

    upgrade to pftf/RPi4 1.33

--- provision-dependencies.sh
+++ provision-dependencies.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # install the dependencies.
-# see https://github.com/pftf/RPi4/blob/v1.32/.github/workflows/linux_edk2.yml
+# see https://github.com/pftf/RPi4/blob/v1.33/.github/workflows/linux_edk2.yml
 apt-get install -y \
     acpica-tools gcc-aarch64-linux-gnu python3-distutils uuid-dev \
     build-essential dos2unix unzip zip
