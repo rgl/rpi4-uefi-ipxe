b7cdc87063d4313a22e53f9669081f035030ba2a
Rui Lopes <rgl@ruilopes.com>
2024-03-14 06:43:33 +0000

    upgrade to virt-firmware 24.2

--- provision-dependencies.sh
+++ provision-dependencies.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 # virt-firmware.
 # see https://pypi.org/project/virt-firmware
 # see https://gitlab.com/kraxel/virt-firmware
-VIRT_FIRMWARE_PIP_INSTALL_SPEC='virt-firmware==23.11'
-VIRT_FIRMWARE_PIP_INSTALL_SPEC='git+https://gitlab.com/kraxel/virt-firmware.git@1b7677e533150da71ed2960b7758e161e1ec6504' # 2023-11-30T12:31:51Z
+VIRT_FIRMWARE_PIP_INSTALL_SPEC='virt-firmware==24.2'
+#VIRT_FIRMWARE_PIP_INSTALL_SPEC='git+https://gitlab.com/kraxel/virt-firmware.git@84a16fe2907ef56a2609e7bfa8235a85bcf2f59e' # 2024-02-15T22:56:01+01:00
 
 # install the dependencies.
-# see https://github.com/pftf/RPi4/blob/v1.35/.github/workflows/linux_edk2.yml
+# see https://github.com/pftf/RPi4/blob/v1.36/.github/workflows/linux_edk2.yml
 apt-get install -y \
     acpica-tools gcc-aarch64-linux-gnu python3-distutils uuid-dev \
     build-essential dos2unix unzip zip
