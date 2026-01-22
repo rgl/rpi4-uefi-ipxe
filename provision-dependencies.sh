a53fecaca2cd9b40bff6f4fb65befc7843ed4f3b
Rui Lopes <rgl@ruilopes.com>
2026-01-22 08:22:22 +0000

    upgrade to virt-firmware 25.12

--- provision-dependencies.sh
+++ provision-dependencies.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 # virt-firmware.
 # see https://pypi.org/project/virt-firmware
 # see https://gitlab.com/kraxel/virt-firmware
-VIRT_FIRMWARE_PIP_INSTALL_SPEC='virt-firmware==24.7'
-#VIRT_FIRMWARE_PIP_INSTALL_SPEC='git+https://gitlab.com/kraxel/virt-firmware.git@7ce1531dbde6d3949cf0f388b652ed2f8e3c9371' # 2024-07-11T15:17:56Z
+VIRT_FIRMWARE_PIP_INSTALL_SPEC='virt-firmware==25.12'
+#VIRT_FIRMWARE_PIP_INSTALL_SPEC='git+https://gitlab.com/kraxel/virt-firmware.git@dc4d64c793823a4edee80c55726744ec48243211' # 2025-12-08T11:19:42Z
 
 # install the dependencies.
 # see https://github.com/pftf/RPi4/blob/v1.38/.github/workflows/linux_edk2.yml
