f1352720bca4b9c848c1ffb3bf498f935fe860a3
Rui Lopes <rgl@ruilopes.com>
2026-01-22 08:22:22 +0000

    update the build dependencies to ubuntu 24.04

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure('2') do |config|
-  config.vm.box = 'ubuntu-22.04-amd64'
+  config.vm.box = 'ubuntu-24.04-uefi-amd64'
 
   config.vm.provider :libvirt do |lv, config|
     lv.memory = 4*1024
--- provision-dependencies.sh
+++ provision-dependencies.sh
@@ -10,10 +10,17 @@ VIRT_FIRMWARE_PIP_INSTALL_SPEC='virt-firmware==24.7'
 # install the dependencies.
 # see https://github.com/pftf/RPi4/blob/v1.38/.github/workflows/linux_edk2.yml
 apt-get install -y \
-    acpica-tools gcc-aarch64-linux-gnu python3-distutils uuid-dev \
+    acpica-tools gcc-aarch64-linux-gnu python3-pip python3-venv uuid-dev \
     build-essential dos2unix unzip zip
 ln -fs /usr/bin/python{3,} # symlink python to python3.
 
+# install venv.
+python3 -m venv --system-site-packages /opt/venv
+echo 'export PATH="/opt/venv/bin:$PATH"' >>~/.bash_login
+export PATH="/opt/venv/bin:$PATH"
+
 # install virt-firmware.
-apt-get install -y python3-pip
 python3 -m pip install $VIRT_FIRMWARE_PIP_INSTALL_SPEC
+
+# show the full path for the virt-fw-vars virt-firmware command.
+which virt-fw-vars
